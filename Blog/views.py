ee0af47e7586cdcc1248871a7fd99f12647910b2
Vasiukhnevich <maksim.vasiukhenvich@gmail.com>
2024-04-08 21:23:19 +0200

    Maksim

--- Blog/views.py
+++ Blog/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Post
 
-"""Вывод записи на главный экран"""
+
 class PostView(View):
+    '''Вывод записи на главный экран'''
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'Blog/main_screen.html', {'post_list': posts})
+
+class PostDetail(View):
+    '''Отдельная страница записи'''
+    def get(self, request, pk):
+        post = Post.objects.get(id=pk)
+        return render(request, 'blog/post_detail.html', {'post' : post})
